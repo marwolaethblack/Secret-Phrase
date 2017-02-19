6856c8dc6cdf08eb75808a0f261444a6c8b015ae
Adnan Unal <a.unal677@gmail.com>
2017-02-19 12:04:57 +0100

    Optimized from 1 minute 20 seconds to just around 22 seconds of decryptinh

--- md5/Program.cs
+++ md5/Program.cs
@@ -20,6 +20,79 @@ namespace md5
             Console.ReadKey();
         }
 
+        /* Checks all possible combinations of words that fit the criteria 
+        Criterium 1: The character occurence of the word combination cannot be greater than the character occurence of the anagramPhrase
+        Criterium 2: The length of the word combination cannot be greater than the length of the anagramPhrase
+        Criterium 3: The md5 hash of the word combination has to be the same as one of the solutions */
+        public static void checkAllCombinations(int[] basePhraseCharOccurence)
+        {
+            Console.WriteLine("Searching this might take around 30 seconds");
+            List<string> filteredWords = readAndFilterFile(basePhraseCharOccurence); //Words from the filtered file of all english words
+
+            int[] anagramPhraseCharOccur = CalculateCharacterOccurence(anagramPhrase);
+            int[] _anagramPhraseCOCopy = CalculateCharacterOccurence(anagramPhrase); //Copy needed to reset charOccur to its base state
+            for (int i = 0; i < filteredWords.Count - 2; i++)
+            {
+                int[] word1 = CalculateCharacterOccurence(filteredWords[i]);
+
+                for (int l = 0; l < 26; l++) //Subtract letters of word1 from anagramPhrase
+                {
+                    anagramPhraseCharOccur[l] -= word1[l];
+                }
+
+                replaceArrays(ref _anagramPhraseCOCopy, anagramPhraseCharOccur);
+                for (int j = i + 1; j < filteredWords.Count - 1; j++)
+                {
+                    int[] word2 = CalculateCharacterOccurence(filteredWords[j]);
+
+                    bool flag = false;
+
+                    for (int l = 0; l < 26; l++) //checks if the letters from word2 can be subtracted from the letters of anagramPhrase
+                    {
+                        if ((anagramPhraseCharOccur[l] - word2[l]) < 0)
+                        {
+                            flag = true; //if not raise flag and continue
+                            break;
+                        }
+                    }
+
+                    if (flag == false)
+                    {
+                        for (int l = 0; l < 26; l++)
+                        {
+                            anagramPhraseCharOccur[l] -= word2[l];
+                        }
+
+                    }
+                    else
+                    {
+                        replaceArrays(ref anagramPhraseCharOccur, _anagramPhraseCOCopy); //reset anagramPhraseCharOccur and continue
+                        continue;
+                    }
+
+                    for (int k = j + 1; k < filteredWords.Count; k++)
+                    {
+                        int[] word3 = CalculateCharacterOccurence(filteredWords[k]);
+                        for (int n = 0; n < 26; n++)
+                        {
+                            //All the remainig letters in anagramPhaseCharOccur have to be subtracted from word3 so all the letters of the anagram phrase are used up
+                            if (anagramPhraseCharOccur[n] - word3[n] != 0) 
+                            {
+                                break;
+                            }
+                            else if (n == 25)
+                            {
+                                checkSolution1(filteredWords[i], filteredWords[j], filteredWords[k]);
+                                checkSolution2(filteredWords[k], filteredWords[i], filteredWords[j]);
+                            }
+                        }
+                    }
+                }
+
+                replaceArrays(ref anagramPhraseCharOccur, basePhraseCharOccurence); //reset character occurence to default
+            }
+        }
+
 
 
         public static string CreateMD5(string input)
@@ -90,7 +163,7 @@ namespace md5
                 for (int i = 0; i < 26; i++)
                 {
                     //Checks if the word can be constructed from the letters of the anagram phrase
-                    if (wordCharOccurence[i] > basePhraseCharOccurence[i]) 
+                    if (wordCharOccurence[i] > basePhraseCharOccurence[i])
                     {
                         break;
                     }
@@ -109,8 +182,6 @@ namespace md5
 
         public static void checkSolution1(string word1, string word2, string word3)
         {
-            if ((word1  + word2 + word3).Length == anagramPhrase.Length)
-            {
                 var md5Hash = CreateMD5(word1 + " " + word2 + " " + word3);
                 if (md5Hash == "e4820b45d2277f3844eac66c903e84be".ToUpper())
                 {
@@ -118,13 +189,11 @@ namespace md5
                     Console.WriteLine(md5Hash);
                 }
 
-            }
+            
         }
 
         public static void checkSolution2(string word1, string word2, string word3)
         {
-            if ((word1 + word2 + word3).Length  == anagramPhrase.Length)
-            {
                 var md5Hash = CreateMD5(word1 + " " + word2 + " " + word3);
                 if (md5Hash == "23170acc097c24edb98fc5488ab033fe".ToUpper())
                 {
@@ -132,69 +201,10 @@ namespace md5
                     Console.WriteLine(md5Hash);
                 }
 
-            }
+            
         }
 
-        /* Checks all possible combinations of words that fit the criteria 
-         Criterium 1: The character occurence of the word combination cannot be greater than the character occurence of the anagramPhrase
-         Criterium 2: The length of the word combination cannot be greater than the length of the anagramPhrase
-         Criterium 3: The md5 hash of the word combination has to be the same as one of the solutions */
-        public static void checkAllCombinations(int[] basePhraseCharOccurence)
-        {
-            Console.WriteLine("Searching this might take a minute or two");
-            List<string> filteredWords = readAndFilterFile(basePhraseCharOccurence); //Words from the filtered file of all english words
-
-            int[] anagramPhraseCharOccur = CalculateCharacterOccurence(anagramPhrase);
-            int[] _anagramPhraseCOCopy = CalculateCharacterOccurence(anagramPhrase); //Copy needed to reset charOccur to its base state
-            for (int i = 0; i < filteredWords.Count - 2; i++)
-            {
-                int[] word1 = CalculateCharacterOccurence(filteredWords[i]);
-
-                for (int l = 0; l < 26; l++) //Subtract letters of word1 from anagramPhrase
-                {
-                    anagramPhraseCharOccur[l] -= word1[l];
-                }
-
-                replaceArrays(ref _anagramPhraseCOCopy, anagramPhraseCharOccur);
-                for (int j = i + 1; j < filteredWords.Count - 1; j++)
-                {
-                    int[] word2 = CalculateCharacterOccurence(filteredWords[j]);
-
-                    bool flag = false; 
-
-                    for (int l = 0; l < 26; l++) //checks if the letters from word2 can be subtracted from the letters of anagramPhrase
-                    {
-                        if ((anagramPhraseCharOccur[l] - word2[l]) < 0)
-                        {
-                            flag = true; //if not raise flag and continue
-                            break;
-                        }
-                    }
-
-                    if (flag == false)
-                    {
-                        for (int l = 0; l < 26; l++)
-                        {
-                            anagramPhraseCharOccur[l] -= word2[l];
-                        }
-
-                    }
-                    else
-                    {
-                        replaceArrays(ref anagramPhraseCharOccur, _anagramPhraseCOCopy); //reset anagramPhraseCharOccur and continue
-                        continue;
-                    }
-
-                    for (int k = j + 1; k < filteredWords.Count; k++)
-                    {
-                        checkSolution1(filteredWords[i], filteredWords[j], filteredWords[k]);
-                        checkSolution2(filteredWords[k], filteredWords[i], filteredWords[j]);
-                    }
-                }
-
-                replaceArrays(ref anagramPhraseCharOccur, basePhraseCharOccurence); //reset character occurence to default
-            }
-        }
+       
 
 
     }
